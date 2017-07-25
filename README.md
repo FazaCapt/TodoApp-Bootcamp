88f994c7a2503bde634c35d45c1cc1443d45a629
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-07-25 13:36:15 +0700

    Todo List: Deleting Todos

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+
+
+
+### Corection:
+
+1. https://www.udemy.com/the-web-developer-bootcamp/learn/v4/t/lecture/5612726?start=0
--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,40 +1,32 @@
 // Check off specific todos by clicking 
 
-/*
-$("li").click(function(){
-    // alert("Clicked Li");
-    // $("li").css("color", "gray"); // Kalo pake cara ini sekali di klik jadi semua yang berubah
-    $(this).css("color", "gray"); // cuma yang li yang di click aja yang berubah
-    $(this).css("text-decoration", "line-through");
-});
-
-*/
-
-// cara lain supaya menjadi single line dan rapih
-
-/*
-$("li").click(function(){
-    // if li is gray
-    if($(this).css("color") === "rgb(128, 128, 128)"){
-        // turn it black
-         $(this).css({
-        color: "black",
-        textDecoration: "none"
-    });
-    }
-    // else
-    else {
-        // turn it gray
-    $(this).css({
-        color: "gray",
-        textDecoration: "line-through"
-    });
-    }
-        
-});
-
-*/
-
 $("li").click(function() {
     $(this).toggleClass("completed");
 })
+
+// Click on X to delete Todo
+$("span").click(function(event){
+    // alert("clicked on a span");
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+}); 
+
+// Cara yang salah, cz akan menampilkan banyak sekali alert
+
+/*
+
+$("ul").click(function(){
+    alert("clicked on a ul");
+});
+
+$("#container").click(function(){
+    alert("clicked on a container div");
+});
+
+$("body").click(function(){
+    alert("clicked on a body");
+});
+
+*/
