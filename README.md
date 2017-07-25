1b5cef8aa37bba8018d8db98ab8606f4a6e0c593
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-07-26 06:47:27 +0700

    Todo List: Styling CSS

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 ### Corection:
 
 1. https://www.udemy.com/the-web-developer-bootcamp/learn/v4/t/lecture/5612726?start=0
+2. Font Roboto: https://fonts.google.com/specimen/Roboto?selection.family=Roboto:400,500,700
+3. uiGradient: https://uigradients.com/#SunnyDays
+4. CDN Font-awesome: https://cdnjs.com/libraries/font-awesome
+5. Icon font awesome: http://fontawesome.io/icons/
--- assets/js/todos.js
+++ assets/js/todos.js
@@ -14,16 +14,9 @@ $("ul").on("click", "span", function(event){
 }); 
 
 $("input[type='text']").keypress(function(event){
-    // console.log("Key Press!");
     if(event.which === 13){
-        // console.log("You hit enter!");
-        // console.log($(this).val());
-        
-        // Grabbing new todo text from input
         var todoText = $(this).val();
         $(this).val("");
-        // Create a new li and add to ul
-        // $("ul").append("<li>THIS IS A NEW LI THAT WE APPENDED TO THIS UL <li>");
-        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
 });
